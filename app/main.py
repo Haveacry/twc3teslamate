cf573b15fc5c246f8458bb186979a28338054322
speedst3r <dean.holland@gmail.com>
2025-04-21 17:52:08 +0800

    Cast MQTT_PORT to integer (#8)

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 # environment variables
 MQTT_HOST = os.getenv('MQTT_HOST', None)
-MQTT_PORT = os.getenv('MQTT_PORT', 1883)
+MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
 MQTT_USERNAME = os.getenv('MQTT_USERNAME', None)
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', None)
 TESLAMATE_CAR_ID = os.getenv('TESLAMATE_CAR_ID', 1)
